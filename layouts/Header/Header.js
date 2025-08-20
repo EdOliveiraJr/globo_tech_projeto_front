800aa13474ba1d3a2560e42b5589c2ba84fc0da9
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-08-20 16:01:24 -0300

    feat: atualizando os caminhos dos arquivos assets

--- layouts/Header/Header.js
+++ layouts/Header/Header.js
@@ -2,7 +2,7 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header class="header">
-        <img src="/assets/images/logo.png" width="150px" alt="Logo da Empresa" class="logo">
+        <img src="./assets/images/logo.png" width="150px" alt="Logo da Empresa" class="logo">
         <div class="user-info">
           <span>Fulano de tal</span>
           <span><i id="logout-icon" class="material-icons">logout</i></span>
