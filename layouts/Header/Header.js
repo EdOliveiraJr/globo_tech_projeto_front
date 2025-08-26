ff0042c671962f3020c2cbcccc8eaa0d2fd43450
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-08-25 22:31:24 -0300

    feat: atualizado caminhos de arquivos e melhorada a navegação entre páginas

--- layouts/Header/Header.js
+++ layouts/Header/Header.js
@@ -2,7 +2,7 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header class="header">
-        <img src="assets/images/logo.png" width="150px" alt="Logo da Empresa" class="logo">
+        <img src="/assets/images/logo.png" width="150px" alt="Logo da Empresa" class="logo">
         <div class="user-info">
           <span>Fulano de tal</span>
           <span><i id="logout-icon" class="material-icons">logout</i></span>
@@ -11,7 +11,7 @@ class Header extends HTMLElement {
     `;
 
     this.querySelector("#logout-icon").addEventListener("click", () => {
-      window.location.replace("/login.html");
+      window.location.replace("/pages/Login/Login.html");
     });
   }  
 }
